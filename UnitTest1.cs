6db1482e748eca69ad67ac70f0c64b129f551bb4
Petru <40626005+petrustia@users.noreply.github.com>
2019-06-16 18:06:24 +0300

    Unit Test
    Unit Test program

---
+++ UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OperationFile;
+
+namespace CheckOpFiles.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WeekDayOccReturns22()
+        {
+            FileOps mondayToFriday = new FileOps();
+
+            int result = mondayToFriday.getWeekDayOccurences("3Mi");
+
+            Assert.AreEqual(22, result);
+        }
+
+        [TestMethod]
+        public void WeekEndOccReturns24()
+        {
+            FileOps saturdayAndSunday = new FileOps();
+
+            int result = saturdayAndSunday.getWeekEndOccurences("7D");
+
+            Assert.AreEqual(24, result);
+        }
+    }
+}
