dd17cd1f70ed59a2e2a82f438cf35a2718ae6328
Petru <40626005+petrustia@users.noreply.github.com>
2019-06-24 11:39:44 +0300

    Upload bat file
    The file which has command for radio scheduler

---
+++ scheduler.bat
@@ -0,0 +1,480 @@
+rem LUNI
+rem ORA 00:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_00" -out=E:\Playlisturi\ORAR\1L\Iun17-00.m3u8
+
+rem ORA 01:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_01" -out=E:\Playlisturi\ORAR\1L\Iun17-01.m3u8
+
+rem ORA 02:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_02" -out=E:\Playlisturi\ORAR\1L\Iun17-02.m3u8
+
+rem ORA 03:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_03" -out=E:\Playlisturi\ORAR\1L\Iun17-03.m3u8
+
+rem ORA 04:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_04" -out=E:\Playlisturi\ORAR\1L\Iun17-04.m3u8
+
+rem ORA 05:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_05" -out=E:\Playlisturi\ORAR\1L\Iun17-05.m3u8
+
+rem ORA 06:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_06" -out=E:\Playlisturi\ORAR\1L\Iun17-06.m3u8
+
+rem ORA 07:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_07" -out=E:\Playlisturi\ORAR\1L\Iun17-07.m3u8
+
+rem ORA 10:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_10" -out=E:\Playlisturi\ORAR\1L\Iun17-10.m3u8
+
+rem ORA 11:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_11a_L-S" -out=E:\Playlisturi\ORAR\1L\Iun17-11.m3u8
+
+rem ORA 12:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_12" -out=E:\Playlisturi\ORAR\1L\Iun17-12.m3u8
+
+rem ORA 13:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_13a_L-V" -out=E:\Playlisturi\ORAR\1L\Iun17-13.m3u8
+
+rem ORA 14:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_14" -out=E:\Playlisturi\ORAR\1L\Iun17-14.m3u8
+
+rem ORA 15:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_15a_L-V" -out=E:\Playlisturi\ORAR\1L\Iun17-15.m3u8
+
+rem ORA 16:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_16a_L-V" -out=E:\Playlisturi\ORAR\1L\Iun17-16.m3u8
+
+rem ORA 17:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_17" -out=E:\Playlisturi\ORAR\1L\Iun17-17.m3u8
+
+rem ORA 18:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_18a_L-V" -out=E:\Playlisturi\ORAR\1L\Iun17-18.m3u8
+
+rem ORA 19:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_19" -out=E:\Playlisturi\ORAR\1L\Iun17-19.m3u8
+
+rem ORA 20:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_20a_L-V" -out=E:\Playlisturi\ORAR\1L\Iun17-20.m3u8
+
+rem ORA 28:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_21a_L-V" -out=E:\Playlisturi\ORAR\1L\Iun17-21.m3u8
+
+rem ORA 29:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_22" -out=E:\Playlisturi\ORAR\1L\Iun17-22.m3u8
+
+rem ORA 30:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_23a_L-V" -out=E:\Playlisturi\ORAR\1L\Iun17-23.m3u8
+
+rem MARTI
+rem ORA 00:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_00" -out=E:\Playlisturi\ORAR\2Ma\Iun18-00.m3u8
+
+rem ORA 01:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_01" -out=E:\Playlisturi\ORAR\2Ma\Iun18-01.m3u8
+
+rem ORA 02:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_02" -out=E:\Playlisturi\ORAR\2Ma\Iun18-02.m3u8
+
+rem ORA 03:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_03" -out=E:\Playlisturi\ORAR\2Ma\Iun18-03.m3u8
+
+rem ORA 04:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_04" -out=E:\Playlisturi\ORAR\2Ma\Iun18-04.m3u8
+
+rem ORA 05:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_05" -out=E:\Playlisturi\ORAR\2Ma\Iun18-05.m3u8
+
+rem ORA 06:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_06" -out=E:\Playlisturi\ORAR\2Ma\Iun18-06.m3u8
+
+rem ORA 07:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_07" -out=E:\Playlisturi\ORAR\2Ma\Iun18-07.m3u8
+
+rem ORA 10:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_10" -out=E:\Playlisturi\ORAR\2Ma\Iun18-10.m3u8
+
+rem ORA 11:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_11a_L-S" -out=E:\Playlisturi\ORAR\2Ma\Iun18-11.m3u8
+
+rem ORA 12:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_12" -out=E:\Playlisturi\ORAR\2Ma\Iun18-12.m3u8
+
+rem ORA 13:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_13a_L-V" -out=E:\Playlisturi\ORAR\2Ma\Iun18-13.m3u8
+
+rem ORA 14:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_14" -out=E:\Playlisturi\ORAR\2Ma\Iun18-14.m3u8
+
+rem ORA 15:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_15a_L-V" -out=E:\Playlisturi\ORAR\2Ma\Iun18-15.m3u8
+
+rem ORA 16:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_16a_L-V" -out=E:\Playlisturi\ORAR\2Ma\Iun18-16.m3u8
+
+rem ORA 17:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_17" -out=E:\Playlisturi\ORAR\2Ma\Iun18-17.m3u8
+
+rem ORA 18:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_18a_L-V" -out=E:\Playlisturi\ORAR\2Ma\Iun18-18.m3u8
+
+rem ORA 19:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_19" -out=E:\Playlisturi\ORAR\2Ma\Iun18-19.m3u8
+
+rem ORA 20:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_20a_L-V" -out=E:\Playlisturi\ORAR\2Ma\Iun18-20.m3u8
+
+rem ORA 28:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_21a_L-V" -out=E:\Playlisturi\ORAR\2Ma\Iun18-21.m3u8
+
+rem ORA 29:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_22" -out=E:\Playlisturi\ORAR\2Ma\Iun18-22.m3u8
+
+rem ORA 30:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_23a_L-V" -out=E:\Playlisturi\ORAR\2Ma\Iun18-23.m3u8
+
+rem MIERCURI
+rem ORA 00:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_00" -out=E:\Playlisturi\ORAR\3Mi\Iun19-00.m3u8
+
+rem ORA 01:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_01" -out=E:\Playlisturi\ORAR\3Mi\Iun19-01.m3u8
+
+rem ORA 02:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_02" -out=E:\Playlisturi\ORAR\3Mi\Iun19-02.m3u8
+
+rem ORA 03:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_03" -out=E:\Playlisturi\ORAR\3Mi\Iun19-03.m3u8
+
+rem ORA 04:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_04" -out=E:\Playlisturi\ORAR\3Mi\Iun19-04.m3u8
+
+rem ORA 05:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_05" -out=E:\Playlisturi\ORAR\3Mi\Iun19-05.m3u8
+
+rem ORA 06:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_06" -out=E:\Playlisturi\ORAR\3Mi\Iun19-06.m3u8
+
+rem ORA 07:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_07" -out=E:\Playlisturi\ORAR\3Mi\Iun19-07.m3u8
+
+rem ORA 10:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_10" -out=E:\Playlisturi\ORAR\3Mi\Iun19-10.m3u8
+
+rem ORA 11:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_11a_L-S" -out=E:\Playlisturi\ORAR\3Mi\Iun19-11.m3u8
+
+rem ORA 12:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_12" -out=E:\Playlisturi\ORAR\3Mi\Iun19-12.m3u8
+
+rem ORA 13:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_13a_L-V" -out=E:\Playlisturi\ORAR\3Mi\Iun19-13.m3u8
+
+rem ORA 14:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_14" -out=E:\Playlisturi\ORAR\3Mi\Iun19-14.m3u8
+
+rem ORA 15:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_15a_L-V" -out=E:\Playlisturi\ORAR\3Mi\Iun19-15.m3u8
+
+rem ORA 16:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_16a_L-V" -out=E:\Playlisturi\ORAR\3Mi\Iun19-16.m3u8
+
+rem ORA 17:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_17" -out=E:\Playlisturi\ORAR\3Mi\Iun19-17.m3u8
+
+rem ORA 18:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_18a_L-V" -out=E:\Playlisturi\ORAR\3Mi\Iun19-18.m3u8
+
+rem ORA 19:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_19" -out=E:\Playlisturi\ORAR\3Mi\Iun19-19.m3u8
+
+rem ORA 20:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_20a_L-V" -out=E:\Playlisturi\ORAR\3Mi\Iun19-20.m3u8
+
+rem ORA 28:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_21a_L-V" -out=E:\Playlisturi\ORAR\3Mi\Iun19-21.m3u8
+
+rem ORA 29:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_22" -out=E:\Playlisturi\ORAR\3Mi\Iun19-22.m3u8
+
+rem ORA 30:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_23a_L-V" -out=E:\Playlisturi\ORAR\3Mi\Iun19-23.m3u8
+
+rem JOI
+rem ORA 00:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_00" -out=E:\Playlisturi\ORAR\4J\Iun20-00.m3u8
+
+rem ORA 01:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_01" -out=E:\Playlisturi\ORAR\4J\Iun20-01.m3u8
+
+rem ORA 02:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_02" -out=E:\Playlisturi\ORAR\4J\Iun20-02.m3u8
+
+rem ORA 03:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_03" -out=E:\Playlisturi\ORAR\4J\Iun20-03.m3u8
+
+rem ORA 04:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_04" -out=E:\Playlisturi\ORAR\4J\Iun20-04.m3u8
+
+rem ORA 05:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_05" -out=E:\Playlisturi\ORAR\4J\Iun20-05.m3u8
+
+rem ORA 06:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_06" -out=E:\Playlisturi\ORAR\4J\Iun20-06.m3u8
+
+rem ORA 07:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_07" -out=E:\Playlisturi\ORAR\4J\Iun20-07.m3u8
+
+rem ORA 10:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_10" -out=E:\Playlisturi\ORAR\4J\Iun20-10.m3u8
+
+rem ORA 11:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_11a_L-S" -out=E:\Playlisturi\ORAR\4J\Iun20-11.m3u8
+
+rem ORA 12:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_12" -out=E:\Playlisturi\ORAR\4J\Iun20-12.m3u8
+
+rem ORA 13:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_13a_L-V" -out=E:\Playlisturi\ORAR\4J\Iun20-13.m3u8
+
+rem ORA 14:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_14" -out=E:\Playlisturi\ORAR\4J\Iun20-14.m3u8
+
+rem ORA 15:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_15a_L-V" -out=E:\Playlisturi\ORAR\4J\Iun20-15.m3u8
+
+rem ORA 16:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_16a_L-V" -out=E:\Playlisturi\ORAR\4J\Iun20-16.m3u8
+
+rem ORA 17:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_17" -out=E:\Playlisturi\ORAR\4J\Iun20-17.m3u8
+
+rem ORA 18:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_18a_L-V" -out=E:\Playlisturi\ORAR\4J\Iun20-18.m3u8
+
+rem ORA 19:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_19" -out=E:\Playlisturi\ORAR\4J\Iun20-19.m3u8
+
+rem ORA 20:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_20a_L-V" -out=E:\Playlisturi\ORAR\4J\Iun20-20.m3u8
+
+rem ORA 28:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_21a_L-V" -out=E:\Playlisturi\ORAR\4J\Iun20-21.m3u8
+
+rem ORA 29:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_22" -out=E:\Playlisturi\ORAR\4J\Iun20-22.m3u8
+
+rem ORA 30:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_23a_L-V" -out=E:\Playlisturi\ORAR\4J\Iun20-23.m3u8
+
+rem VINERI
+rem ORA 00:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_00" -out=E:\Playlisturi\ORAR\5V\Iun21-00.m3u8
+
+rem ORA 01:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_01" -out=E:\Playlisturi\ORAR\5V\Iun21-01.m3u8
+
+rem ORA 02:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_02" -out=E:\Playlisturi\ORAR\5V\Iun21-02.m3u8
+
+rem ORA 03:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_03" -out=E:\Playlisturi\ORAR\5V\Iun21-03.m3u8
+
+rem ORA 04:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_04" -out=E:\Playlisturi\ORAR\5V\Iun21-04.m3u8
+
+rem ORA 05:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_05" -out=E:\Playlisturi\ORAR\5V\Iun21-05.m3u8
+
+rem ORA 06:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_06" -out=E:\Playlisturi\ORAR\5V\Iun21-06.m3u8
+
+rem ORA 07:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_07" -out=E:\Playlisturi\ORAR\5V\Iun21-07.m3u8
+
+rem ORA 10:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_10" -out=E:\Playlisturi\ORAR\5V\Iun21-10.m3u8
+
+rem ORA 11:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_11a_L-S" -out=E:\Playlisturi\ORAR\5V\Iun21-11.m3u8
+
+rem ORA 12:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_12" -out=E:\Playlisturi\ORAR\5V\Iun21-12.m3u8
+
+rem ORA 13:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_13a_L-V" -out=E:\Playlisturi\ORAR\5V\Iun21-13.m3u8
+
+rem ORA 14:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_14" -out=E:\Playlisturi\ORAR\5V\Iun21-14.m3u8
+
+rem ORA 15:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_15a_L-V" -out=E:\Playlisturi\ORAR\5V\Iun21-15.m3u8
+
+rem ORA 16:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_16a_L-V" -out=E:\Playlisturi\ORAR\5V\Iun21-16.m3u8
+
+rem ORA 17:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_17" -out=E:\Playlisturi\ORAR\5V\Iun21-17.m3u8
+
+rem ORA 18:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_18a_L-V" -out=E:\Playlisturi\ORAR\5V\Iun21-18.m3u8
+
+rem ORA 19:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_19" -out=E:\Playlisturi\ORAR\5V\Iun21-19.m3u8
+
+rem ORA 20:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_20a_L-V" -out=E:\Playlisturi\ORAR\5V\Iun21-20.m3u8
+
+rem ORA 28:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_21a_L-V" -out=E:\Playlisturi\ORAR\5V\Iun21-21.m3u8
+
+rem ORA 29:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_22" -out=E:\Playlisturi\ORAR\5V\Iun21-22.m3u8
+
+rem ORA 30:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_23a_L-V" -out=E:\Playlisturi\ORAR\5V\Iun21-23.m3u8
+
+rem SAMBATA
+rem ORA 00:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_00" -out=E:\Playlisturi\ORAR\6S\Iun22-00.m3u8
+
+rem ORA 01:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_01" -out=E:\Playlisturi\ORAR\6S\Iun22-01.m3u8
+
+rem ORA 02:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_02" -out=E:\Playlisturi\ORAR\6S\Iun22-02.m3u8
+
+rem ORA 03:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_03" -out=E:\Playlisturi\ORAR\6S\Iun22-03.m3u8
+
+rem ORA 04:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_04" -out=E:\Playlisturi\ORAR\6S\Iun22-04.m3u8
+
+rem ORA 05:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_05" -out=E:\Playlisturi\ORAR\6S\Iun22-05.m3u8
+
+rem ORA 06:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_06" -out=E:\Playlisturi\ORAR\6S\Iun22-06.m3u8
+
+rem ORA 07:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_07" -out=E:\Playlisturi\ORAR\6S\Iun22-07.m3u8
+
+rem ORA 08:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_08a_S" -out=E:\Playlisturi\ORAR\6S\Iun22-08.m3u8
+
+rem ORA 09:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_09" -out=E:\Playlisturi\ORAR\6S\Iun22-09.m3u8
+
+rem ORA 10:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_10a_S" -out=E:\Playlisturi\ORAR\6S\Iun22-10.m3u8
+
+rem ORA 11:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_11b_S" -out=E:\Playlisturi\ORAR\6S\Iun22-11.m3u8
+
+rem ORA 12:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_12" -out=E:\Playlisturi\ORAR\6S\Iun22-12.m3u8
+
+rem ORA 13:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_13b_S" -out=E:\Playlisturi\ORAR\6S\Iun22-13.m3u8
+
+rem ORA 14:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_14" -out=E:\Playlisturi\ORAR\6S\Iun22-14.m3u8
+
+rem ORA 15:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_15b_S-D" -out=E:\Playlisturi\ORAR\6S\Iun22-15.m3u8
+
+rem ORA 16:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_16b_S" -out=E:\Playlisturi\ORAR\6S\Iun22-16.m3u8
+
+rem ORA 17:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_17" -out=E:\Playlisturi\ORAR\6S\Iun22-17.m3u8
+
+rem ORA 18:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_18b_S" -out=E:\Playlisturi\ORAR\6S\Iun22-18.m3u8
+
+rem ORA 19:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_19" -out=E:\Playlisturi\ORAR\6S\Iun22-19.m3u8
+
+rem ORA 20:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_20b_S" -out=E:\Playlisturi\ORAR\6S\Iun22-20.m3u8
+
+rem ORA 28:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_21b_S-D" -out=E:\Playlisturi\ORAR\6S\Iun22-21.m3u8
+
+rem ORA 29:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_22" -out=E:\Playlisturi\ORAR\6S\Iun22-22.m3u8
+
+rem ORA 30:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_23b_S-D" -out=E:\Playlisturi\ORAR\6S\Iun22-23.m3u8
+
+rem DUMINICA
+rem ORA 00:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_00" -out=E:\Playlisturi\ORAR\7D\Iun23-00.m3u8
+
+rem ORA 01:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_01" -out=E:\Playlisturi\ORAR\7D\Iun23-01.m3u8
+
+rem ORA 02:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_02" -out=E:\Playlisturi\ORAR\7D\Iun23-02.m3u8
+
+rem ORA 03:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_03" -out=E:\Playlisturi\ORAR\7D\Iun23-03.m3u8
+
+rem ORA 04:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_04" -out=E:\Playlisturi\ORAR\7D\Iun23-04.m3u8
+
+rem ORA 05:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_05" -out=E:\Playlisturi\ORAR\7D\Iun23-05.m3u8
+
+rem ORA 06:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_06" -out=E:\Playlisturi\ORAR\7D\Iun23-06.m3u8
+
+rem ORA 07:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_07" -out=E:\Playlisturi\ORAR\7D\Iun23-07.m3u8
+
+rem ORA 08:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_08b_D" -out=E:\Playlisturi\ORAR\7D\Iun23-08.m3u8
+
+rem ORA 09:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_09" -out=E:\Playlisturi\ORAR\7D\Iun23-09.m3u8
+
+rem ORA 10:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_10b_D" -out=E:\Playlisturi\ORAR\7D\Iun23-10.m3u8
+
+rem ORA 11:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_11c_D" -out=E:\Playlisturi\ORAR\7D\Iun23-11.m3u8
+
+rem ORA 12:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_12" -out=E:\Playlisturi\ORAR\7D\Iun23-12.m3u8
+
+rem ORA 13:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_13c_D" -out=E:\Playlisturi\ORAR\7D\Iun23-13.m3u8
+
+rem ORA 14:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_14" -out=E:\Playlisturi\ORAR\7D\Iun23-14.m3u8
+
+rem ORA 15:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_15b_S-D" -out=E:\Playlisturi\ORAR\7D\Iun23-15.m3u8
+
+rem ORA 16:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_16c_D" -out=E:\Playlisturi\ORAR\7D\Iun23-16.m3u8
+
+rem ORA 17:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_17" -out=E:\Playlisturi\ORAR\7D\Iun23-17.m3u8
+
+rem ORA 18:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_18c_D" -out=E:\Playlisturi\ORAR\7D\Iun23-18.m3u8
+
+rem ORA 19:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_19" -out=E:\Playlisturi\ORAR\7D\Iun23-19.m3u8
+
+rem ORA 20:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_20c_D" -out=E:\Playlisturi\ORAR\7D\Iun23-20.m3u8
+
+rem ORA 28:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_21b_S-D" -out=E:\Playlisturi\ORAR\7D\Iun23-21.m3u8
+
+rem ORA 29:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_22" -out=E:\Playlisturi\ORAR\7D\Iun23-22.m3u8
+
+rem ORA 30:00
+E:\RadioBOSS\PlaylistGeneratorPro.exe "-preset=ORA_23b_S-D" -out=E:\Playlisturi\ORAR\7D\Iun23-23.m3u8
